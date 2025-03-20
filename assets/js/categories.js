08ec13a956dfef7c752af86a2c398c791180de67
unknown <alidshaikh05@gmail.com>
2025-03-20 11:18:07 +0530

    updated the search bar in the catogries sectio

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -22,11 +22,37 @@
         medicine.style.display = 'flex';
       } else {
         medicine.style.display = 'none';
-        const tableBody = document.getElementById("product-grid");
-        tableBody.innerHTML = `<p>No medicines found with the name "${query}".</p>`; 
+       
       }
     });
   }
+
+  // function searchMedicines() {
+  //   const query = document.getElementById('searchInput').value.toLowerCase();
+  //   const medicines = document.querySelectorAll('.medicine-card');
+  //   let found = false; // To track if any medicine matches the query
+  
+  //   medicines.forEach((medicine) => {
+  //     if (medicine.textContent.toLowerCase().includes(query)) {
+  //       medicine.style.display = 'flex'; // Show matching medicine
+  //       found = true;
+  //     } else {
+  //       medicine.style.display = 'none'; // Hide non-matching medicine
+  //     }
+  //   });
+  
+  //   const tableBody = document.getElementById("product-grid");
+  //   if (!found) {
+  //     tableBody.innerHTML = `<p>No medicines found with the name "${query}".</p>`;
+  //   } else {
+  //     // If medicines are found, ensure the "No medicines found" message is removed
+  //     const noMedicinesMessage = tableBody.querySelector('p');
+  //     if (noMedicinesMessage) {
+  //       noMedicinesMessage.remove();
+  //     }
+  //   }
+  // }
+  
 
   
   
