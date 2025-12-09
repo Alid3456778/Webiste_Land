57e499dbd5b6cffb4d09078811613a54548bf783
Ali <alidshaikh05gmail.com>
2025-12-09 19:16:32 +0530

    Update the UI v3

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -413,9 +413,9 @@ if (heroImages.length === 0) {
   function changeHeroImage() {
     currentIndex = (currentIndex + 1) % heroImages.length;
     heroSection.style.backgroundImage = `url(assets/image2/christmas.png)`;
-    heroSection.style.backgroundSize = "cover";
+    heroSection.style.backgroundSize = "contain";
     heroSection.style.backgroundPosition = "center";
-    heroSection.style.height = "80vh";
+    // heroSection.style.height = "80vh";
     heroSection.style.width = "100vw";
   }
 
